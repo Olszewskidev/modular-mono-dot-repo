236a5a3e8e5bce42c58608da4f0c0b9f87cddd34
Kamil <dev.olszewski@gmail.com>
2024-12-27 14:46:31 +0100

    Modular monolith template init.

---
+++ src/Modules/A/ModularMono.Modules.A.Api/ModuleAController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModularMono.Modules.A.Api;
+
+[ApiController]
+[Route("[controller]")]
+public sealed class ModuleAController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult Get()
+        => Ok("Hello from Module A!");
+}
---
+++ src/Modules/B/ModularMono.Modules.B.Api/ModuleBController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModularMono.Modules.B.Api;
+
+[ApiController]
+[Route("[controller]")]
+public sealed class ModuleBController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult Get()
+    => Ok("Hello from Module B!");
+}
